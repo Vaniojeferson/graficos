b6d0352a35a7e280fa70de1fb8d0db0bc6f9ca35
Vaniojeferson <90858830+Vaniojeferson@users.noreply.github.com>
2022-06-07 19:03:24 -0300

    atua

--- index.js
+++ index.js
@@ -19,18 +19,16 @@ var res = document.getElementById("view")
 function logistica(){
  
 
- res.innerHTML = `<iframe title="Postos Ale" width="60%" height="650" src="https://app.powerbi.com/view?r=eyJrIjoiYTAzNTY2YTktM2E4Zi00OWU0LWFmMjktNjVjNzIxZWYzMDBjIiwidCI6ImRkNjg1YzRlLTY2NjQtNDllMy05YTA4LTRkYWFmZTU1Zjk1NSJ9&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>
+ res.innerHTML = `<iframe title="BI - LOGÍSTICA" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiZDQ4MjYyZjUtOTUyYi00YzI0LWE3OTctOWI4YmZmNjZlYzU0IiwidCI6ImRkNjg1YzRlLTY2NjQtNDllMy05YTA4LTRkYWFmZTU1Zjk1NSJ9&pageName=ReportSection933ad2573e5e00c3ebd6" frameborder="0" allowFullScreen="true"></iframe>
  
  <button onclick="apagar()" class="fechar">X Fechar</button>`
  
- 
-
 }
 
 function enel(){
  
 
-    res.innerHTML = `<iframe title="Enel" width="60%" height="650" src="https://app.powerbi.com/view?r=eyJrIjoiYzM2NmM4MTMtNDdjYy00Yzc5LThlZTYtOWFhZGU1ZGIzZjE1IiwidCI6ImRkNjg1YzRlLTY2NjQtNDllMy05YTA4LTRkYWFmZTU1Zjk1NSJ9&pageName=ReportSection09f8c436fa96045848fe" frameborder="0" allowFullScreen="true"></iframe>
+    res.innerHTML = `<iframe title="Enel" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiYzM2NmM4MTMtNDdjYy00Yzc5LThlZTYtOWFhZGU1ZGIzZjE1IiwidCI6ImRkNjg1YzRlLTY2NjQtNDllMy05YTA4LTRkYWFmZTU1Zjk1NSJ9&pageName=ReportSection09f8c436fa96045848fe" frameborder="0" allowFullScreen="true"></iframe>
     
     <button onclick="apagar()" class="fechar">X Fechar</button>`
    
@@ -40,7 +38,7 @@ function enel(){
 function cpfl(){
  
 
-    res.innerHTML = `<iframe title="BI - CPFL" width="60%" height="650" src="https://app.powerbi.com/view?r=eyJrIjoiMjc2MTlmNjktNzBlMi00M2I1LWIwNzYtZTAxOGQ3MWI5NGIzIiwidCI6ImRkNjg1YzRlLTY2NjQtNDllMy05YTA4LTRkYWFmZTU1Zjk1NSJ9&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>
+    res.innerHTML = `<iframe title="BI - CPFL" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMjc2MTlmNjktNzBlMi00M2I1LWIwNzYtZTAxOGQ3MWI5NGIzIiwidCI6ImRkNjg1YzRlLTY2NjQtNDllMy05YTA4LTRkYWFmZTU1Zjk1NSJ9&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>
     
     <button onclick="apagar()" class="fechar">X Fechar</button>`
    
@@ -50,7 +48,7 @@ function cpfl(){
 function cpflCien(){
  
 
-    res.innerHTML = `<iframe title="Projeto CPFL" width="60%" height="650" src="https://app.powerbi.com/view?r=eyJrIjoiNmZiODUwZjAtYmUwZi00MTIxLTg0OWQtMjM2NjFlNzkwMmFjIiwidCI6ImRkNjg1YzRlLTY2NjQtNDllMy05YTA4LTRkYWFmZTU1Zjk1NSJ9&pageName=ReportSectionb8cf769062235a0a0d59" frameborder="0" allowFullScreen="true"></iframe>
+    res.innerHTML = `<iframe title="Projeto CPFL" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiNmZiODUwZjAtYmUwZi00MTIxLTg0OWQtMjM2NjFlNzkwMmFjIiwidCI6ImRkNjg1YzRlLTY2NjQtNDllMy05YTA4LTRkYWFmZTU1Zjk1NSJ9&pageName=ReportSectionb8cf769062235a0a0d59" frameborder="0" allowFullScreen="true"></iframe>
     
     <button onclick="apagar()" class="fechar">X Fechar</button>`
    
@@ -61,7 +59,7 @@ function corinthians(){
 
     res.innerHTML = `
     
-    <iframe title="Postos Ale" width="60%" height="650" src="https://app.powerbi.com/view?r=eyJrIjoiYTAzNTY2YTktM2E4Zi00OWU0LWFmMjktNjVjNzIxZWYzMDBjIiwidCI6ImRkNjg1YzRlLTY2NjQtNDllMy05YTA4LTRkYWFmZTU1Zjk1NSJ9&pageName=ReportSection0c340ee86303c0958d52" frameborder="0" allowFullScreen="true"></iframe>
+    <iframe title="Postos Ale" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiYTAzNTY2YTktM2E4Zi00OWU0LWFmMjktNjVjNzIxZWYzMDBjIiwidCI6ImRkNjg1YzRlLTY2NjQtNDllMy05YTA4LTRkYWFmZTU1Zjk1NSJ9&pageName=ReportSection0c340ee86303c0958d52" frameborder="0" allowFullScreen="true"></iframe>
     <button onclick="apagar()" class="fechar">X Fechar</button> `
    
 }
